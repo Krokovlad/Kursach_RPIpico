662274993cc2b753744862bac41cc696a9cfca8b
Krokovlad <vladik.channel17@gmail.com>
2022-12-19 02:39:40 +0300

    Added RFID support

--- src/main.cpp
+++ src/main.cpp
@@ -1,80 +1,42 @@
-/***************************************************
-  This is an example sketch for our optical Fingerprint sensor
-
-  Designed specifically to work with the Adafruit BMP085 Breakout
-  ----> http://www.adafruit.com/products/751
-
-  These displays use TTL Serial to communicate, 2 pins are required to
-  interface
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
 #include <Arduino.h>
+#include <PN532.h>
+#include <PN532_HSU.h>
 #include <Adafruit_Fingerprint.h>
-//#include <SoftwareSerial.h>
-
-//#if (defined(__AVR__) || defined(ESP8266)) && !defined(__AVR_ATmega2560__)
-// For UNO and others without hardware serial, we must use software serial...
-// pin #2 is IN from sensor (GREEN wire)
-// pin #3 is OUT from arduino  (WHITE wire)
-// Set up the serial port to use softwareserial..
-//SoftwareSerial mySerial(1, 3);
-
-//#else
-// On Leonardo/M0/etc, others with hardware serial, use hardware serial!
-// #0 is green wire, #1 is white
-
-
 #define mySerial Serial1
-//#endif
 
-#include <Wire.h>
-#include <SPI.h>
-#include <Adafruit_PN532.h>
+PN532_HSU pn532hsu(Serial2);
+PN532 nfc(pn532hsu);
 
-// If using the breakout with SPI, define the pins for SPI communication.
-#define PN532_SCK  (18)
-#define PN532_MOSI (21)
-#define PN532_SS   (17)
-#define PN532_MISO (19)
 
-// If using the breakout or shield with I2C, define just the pins connected
-// to the IRQ and reset lines.  Use the values below (2, 3) for the shield!
-#define PN532_IRQ   (14)
-#define PN532_RESET (15)  // Not connected by default on the NFC Shield
-
-// Uncomment just _one_ line below depending on how your breakout or shield
-// is connected to the Arduino:
-
-// Use this line for a breakout with a SPI connection:
-Adafruit_PN532 nfc(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS);
-
-// Use this line for a breakout with a hardware SPI connection.  Note that
-// the PN532 SCK, MOSI, and MISO pins need to be connected to the Arduino's
-// hardware SPI SCK, MOSI, and MISO pins.  On an Arduino Uno these are
-// SCK = 13, MOSI = 11, MISO = 12.  The SS line can be any digital IO pin.
-//Adafruit_PN532 nfc(PN532_SS);
-
-// Or use this line for a breakout or shield with an I2C connection:
-//Adafruit_PN532 nfc(PN532_IRQ, PN532_RESET);
-
-//Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
-
-void setup()
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+String tagId = "None", dispTag = "None";
+byte nuidPICC[4];
+ 
+void setup(void)
 {
-  
-  //Serial.swap();
-  Serial.begin(9600);
-  while (!Serial);  // For Yun/Leo/Micro/Zero/...
+  SerialUSB.begin(115200);
   delay(100);
-
-  /*Serial.println("\n\nAdafruit finger detect test");
-
-  // set the data rate for the sensor serial port
+  SerialUSB.println("Hello Maker!");
+  
+  //  Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);
+  nfc.begin();
+  uint32_t versiondata = nfc.getFirmwareVersion();
+  if (! versiondata)
+  {
+    SerialUSB.print("Didn't Find PN53x Module");
+    //while (1); // Halt
+  }
+  // Got valid data, print it out!
+  SerialUSB.print("Found chip PN5");
+  SerialUSB.println((versiondata >> 24) & 0xFF, HEX);
+  SerialUSB.print("Firmware ver. ");
+  SerialUSB.print((versiondata >> 16) & 0xFF, DEC);
+  SerialUSB.print('.'); 
+  SerialUSB.println((versiondata >> 8) & 0xFF, DEC);
+  // Configure board to read RFID tags
+  nfc.SAMConfig();
+  //Serial.println("Waiting for an ISO14443A Card ...");
+  SerialUSB.println("\n\nAdafruit finger detect test");
   finger.begin(57600);
   delay(5);
   if (finger.verifyPassword()) {
@@ -82,178 +44,71 @@ void setup()
   } else {
     Serial.println("Did not find fingerprint sensor :(");
     //while (1) { delay(1); }
-  }*/
-
-  //Serial.end();
-  //Serial.flush();
-  /*Serial.println(F("Reading sensor parameters"));
-  finger.getParameters();
-  Serial.print(F("Status: 0x")); Serial.println(finger.status_reg, HEX);
-  Serial.print(F("Sys ID: 0x")); Serial.println(finger.system_id, HEX);
-  Serial.print(F("Capacity: ")); Serial.println(finger.capacity);
-  Serial.print(F("Security level: ")); Serial.println(finger.security_level);
-  Serial.print(F("Device address: ")); Serial.println(finger.device_addr, HEX);
-  Serial.print(F("Packet len: ")); Serial.println(finger.packet_len);
-  Serial.print(F("Baud rate: ")); Serial.println(finger.baud_rate);*/
-
-/*
-  finger.getTemplateCount();
-  Serial.begin(9600);
-  if (finger.templateCount == 0) {
-    Serial.print("Sensor doesn't contain any fingerprint data. Please run the 'enroll' example.");
   }
-  else {
-    Serial.println("Waiting for valid finger...");
-      Serial.print("Sensor contains "); Serial.print(finger.templateCount); Serial.println(" templates");
-  }
-*/
 
-  Serial.begin(115200);
-  while (!Serial) delay(10); // for Leonardo/Micro/Zero
-
-  Serial.println("Hello!");
-
-  nfc.begin();
-
-  uint32_t versiondata = nfc.getFirmwareVersion();
-  if (! versiondata) {
-    Serial.print("Didn't find PN53x board");
-    while (1); // halt
-  }
-  // Got ok data, print it out!
-  Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX); 
-  Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC); 
-  Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
-  
-  // configure board to read RFID tags
-  nfc.SAMConfig();
-  
-  Serial.println("Waiting for an ISO14443A Card ...");
 }
-
-/*uint8_t getFingerprintID() {
-  uint8_t p = finger.getImage();
-  switch (p) {
-    case FINGERPRINT_OK:
-      Serial.println("Image taken");
-      break;
-    case FINGERPRINT_NOFINGER:
-      Serial.println("No finger detected");
-      return p;
-    case FINGERPRINT_PACKETRECIEVEERR:
-      Serial.println("Communication error");
-      return p;
-    case FINGERPRINT_IMAGEFAIL:
-      Serial.println("Imaging error");
-      return p;
-    default:
-      Serial.println("Unknown error");
-      return p;
-  }
-
-  // OK success!
-
-  p = finger.image2Tz();
-  switch (p) {
-    case FINGERPRINT_OK:
-      Serial.println("Image converted");
-      break;
-    case FINGERPRINT_IMAGEMESS:
-      Serial.println("Image too messy");
-      return p;
-    case FINGERPRINT_PACKETRECIEVEERR:
-      Serial.println("Communication error");
-      return p;
-    case FINGERPRINT_FEATUREFAIL:
-      Serial.println("Could not find fingerprint features");
-      return p;
-    case FINGERPRINT_INVALIDIMAGE:
-      Serial.println("Could not find fingerprint features");
-      return p;
-    default:
-      Serial.println("Unknown error");
-      return p;
-  }
-
-  // OK converted!
-  p = finger.fingerSearch();
-  if (p == FINGERPRINT_OK) {
-    Serial.println("Found a print match!");
-  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
-    Serial.println("Communication error");
-    return p;
-  } else if (p == FINGERPRINT_NOTFOUND) {
-    Serial.println("Did not find a match");
-    return p;
-  } else {
-    Serial.println("Unknown error");
-    return p;
-  }
-
-  // found a match!
-  //Serial.print("Found ID #"); 
-  Serial.print(finger.fingerID);
-  Serial.print(" with confidence of "); Serial.println(finger.confidence);
-
-  return finger.fingerID;
-}*/
-
-
-void loop()                     // run over and over again
-{ 
-  while(1){
-
-  //getFingerprintID();
-  delay(50);            //don't ned to run this at full speed.
-
-  Serial.print("  waiting ");
-  boolean success;                                                                              // задаём переменную для считывания номера карты
-  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };                                                      // задаём переменную для хранения номера считанной карты
-  uint8_t uidLength;                                                                            // длина номера карты (4 или 7 байт в зависимости от типа карты)
-  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength);
-  Serial.print("still  waiting ");
-  
-  if (success) {
-    // Display some basic information about the card
-    Serial.println("Found an ISO14443A card");
-    Serial.print("  UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
-    Serial.print("  UID Value: ");
-    nfc.PrintHex(uid, uidLength);
-    
-    if (uidLength == 4)
-    {
-      // We probably have a Mifare Classic card ... 
-      uint32_t cardid = uid[0];
-      cardid <<= 8;
-      cardid |= uid[1];
-      cardid <<= 8;
-      cardid |= uid[2];  
-      cardid <<= 8;
-      cardid |= uid[3]; 
-      Serial.print("Seems to be a Mifare Classic card #");
-      Serial.println(cardid);
-    }
-    Serial.println("");
-  }
-  
- }
-}
-
-
-// returns -1 if failed, otherwise returns ID #
-/*int getFingerprintIDez() {
+ 
+ int getFingerprintIDez() {
   uint8_t p = finger.getImage();
   if (p != FINGERPRINT_OK)  return -1;
-
   p = finger.image2Tz();
   if (p != FINGERPRINT_OK)  return -1;
-
   p = finger.fingerFastSearch();
   if (p != FINGERPRINT_OK)  return -1;
-
   // found a match!
   Serial.print("Found ID #"); Serial.print(finger.fingerID);
   Serial.print(" with confidence of "); Serial.println(finger.confidence);
   return finger.fingerID;
-}*/
+}
+ 
+String tagToString(byte id[4])
+{
+  String tagId = "";
+  for (byte i = 0; i < 4; i++)
+  {
+    if (i < 3) tagId += String(id[i]) + ".";
+    else tagId += String(id[i]);
+  }
+  return tagId;
+}
+ 
+ 
+void readNFC()
+{
+  boolean success;
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+  uint8_t uidLength;                       // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength);
+  if (success)
+  {
+    SerialUSB.print("UID Length: ");
+    SerialUSB.print(uidLength, DEC);
+    SerialUSB.println(" bytes");
+    SerialUSB.print("UID Value: ");
+    for (uint8_t i = 0; i < uidLength; i++)
+    {
+      nuidPICC[i] = uid[i];
+      SerialUSB.print(" "); SerialUSB.print(uid[i], DEC);
+    }
+    SerialUSB.println();
+    tagId = tagToString(nuidPICC);
+    dispTag = tagId;
+    SerialUSB.print(F("tagId is : "));
+    SerialUSB.println(tagId);
+    SerialUSB.println("");
+    delay(1000);  // 1 second halt
+  }
+  else
+  {
+    // PN532 probably timed out waiting for a card
+    SerialUSB.println("Timed out! Waiting for a card...");
+  }
+}
+
+void loop()
+{
+  readNFC();
+  getFingerprintIDez();
+  delay(100);
+}
 
